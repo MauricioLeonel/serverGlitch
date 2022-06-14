f9ace7f635e552bea58db653f4252ba0115c8820
mauricio barrios <MLBarrios@teco.com.ar>
2022-06-14 14:45:46 -0300

    modificacion

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 
 
 
-const productos = require('./routers/productos.js')
+const productos = require('./src/routers/productos.js')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
